7dd0cbec7c7ac56d618f911093983c081ab19886
thititongumpun <thiti180536@gmail.com>
2025-07-27 20:17:42 +0700

    feat: add nocodb

--- src/modules/feed/index.ts
+++ src/modules/feed/index.ts
@@ -1,12 +1,27 @@
 import { Elysia } from 'elysia'
 
 import { AirTableService } from './service'
-
+import { Api } from 'nocodb-sdk'
 export const feed = new Elysia({ prefix: '/feed' })
   .get(
     '',
     async () => {
-      const data = await AirTableService.getList()
-      return "ok"
+      // const data = await AirTableService.getList()
+      const api = new Api({
+        baseURL: 'xxxxxxxxxx',
+        headers: {
+          'xc-token': 'asdasdsdasd' // or 'xc-token': '<API_TOKEN>'
+        }
+      });
+
+
+
+
+      // const tables = await api.dbTable.list('pwqy2nqxf377iwy');
+      // console.log('Tables:', tables);
+      const tableData = await api.dbTableRow.list('bkpostthailand', 'pwqy2nqxf377iwy', 'bkpostthailand', {
+        limit: 10
+      })
+      console.log(tableData)
     }
   )
